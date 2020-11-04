a0290060a56cae5592f552dc6d20427914d82ab6
grapeJUICE1 <anonymyty123@gmail.com>
2020-11-04 17:32:34 +0600

    Initial commit

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const server = new ApolloServer({
   context: contextMiddleware,
 });
 
-server.listen().then(({ url }) => {
+server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
   console.log(`🚀 Server ready at ${url}`);
 
   sequelize
